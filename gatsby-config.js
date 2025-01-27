694b3dececcf90b8d92bfd2967b13c7bfdd37cbf
Sidd <sr.sanagala@outlook.com>
2025-01-27 18:47:17 +0800

    Add favicon

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,9 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
-    description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
-    image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    title: 'Siddardha Reddy',
+    description: 'Sidd\'s personal website',
+    siteUrl: 'https://srsanagala.com', // No trailing slash allowed!
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -20,8 +17,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: 'Brittany Chiang',
-        short_name: 'Brittany Chiang',
+        name: 'Siddardha Reddy',
+        short_name: 'Siddardha Reddy',
         start_url: '/',
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
